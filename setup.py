ea1dd5d8b5f79d2ea96a6479e9336fcde0a7cf61
Jakub Szymański <jakubmateusz@poczta.onet.pl>
2019-05-18 13:43:00 +0200

    installation script ready

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from xml.dom.minidom import parse
+import shutil
 
 setup(name='WoeUSB-ng',
       version='0.1',
@@ -18,3 +20,16 @@ setup(name='WoeUSB-ng',
           'wxPython'
       ]
       )
+
+path = "/usr/bin/woeusbgui"
+dom = parse('./polkit/com.github.woeusb.woeusb-ng.policy')
+for action in dom.getElementsByTagName('action'):
+    if action.getAttribute('id') == "com.github.slacka.woeusb.run-gui-using-pkexec":
+        for annotate in action.getElementsByTagName('annotate'):
+            if annotate.getAttribute('key') == "org.freedesktop.policykit.exec.path":
+                annotate.childNodes[0].nodeValue = path
+
+polkit = open('./polkit/com.github.woeusb.woeusb-ng.policy', "w")
+dom.writexml(polkit)
+
+shutil.copy2('./polkit/com.github.woeusb.woeusb-ng.policy', "/usr/share/polkit-1/actions")

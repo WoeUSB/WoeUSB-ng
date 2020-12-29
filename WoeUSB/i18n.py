445789dac7b601fd0c2147f5252711056a43eecd
Jakub Szymański <jakubmateusz@poczta.onet.pl>
2020-12-29 18:29:06 +0100

    Fix another issue with translation

--- WoeUSB/i18n.py
+++ WoeUSB/i18n.py
@@ -1,6 +1,7 @@
 import gettext
+import locale
 import os
 
-translation = gettext.translation("woeusb", os.path.dirname(__file__) + "/locale", ['pl', 'zh', 'fr'], fallback=True)
+translation = gettext.translation("woeusb", os.path.dirname(__file__) + "/locale", [locale.getlocale()[0]], fallback=True)
 translation.install()
 i18n = translation.gettext

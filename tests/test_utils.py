bacafe8bb704e639d21aa9bcf319baacf61945ff
Jakub Szymański <public@waxy.one>
2021-10-25 18:24:46 +0200

    Add UT for check_runtime_parameters

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,23 +1,12 @@
 import os
-import shutil
 import sys
+import shutil
+import pathlib
 
 import pytest
 
 sys.path.append(".")
 import WoeUSB.utils
-
-
-class TestDetermineTargetParameters:
-    def test_device(self):
-        [target_device, target_partition] = WoeUSB.utils.determine_target_parameters("device", "/dev/sda")
-        assert {target_device, target_partition} \
-               .difference(["/dev/sda", "/dev/sda1"]) == set()
-
-    def test_partition(self):
-        [target_device, target_partition] = WoeUSB.utils.determine_target_parameters("partition", "/dev/sda1")
-        assert {target_device, target_partition} \
-               .difference(["/dev/sda", "/dev/sda1"]) == set()
 
 
 class TestCheckRuntimeDependencies:
@@ -116,6 +105,105 @@ class TestCheckRuntimeDependencies:
         [fat, ntfs, grub] = WoeUSB.utils.check_runtime_dependencies("WoeUSB-ng")
 
         assert grub == command
+
+
+class TestCheckRuntimeParameters:
+    path_preserve = None
+    it_is_a_file = ""
+    it_is_a_block_device = []
+
+    class Path:
+        path = ""
+        it_is_a_block_device = ""
+
+        def __init__(self, path):
+            self.path = path
+
+        def is_block_device(self):
+            return self.path in self.it_is_a_block_device
+
+    @pytest.fixture()
+    def setup(self, monkeypatch):
+        monkeypatch.setattr(os.path, "isfile", self.isfile)
+        self.path_preserve = pathlib.Path
+        monkeypatch.setattr(pathlib, "Path", self.path)
+        yield "setup"
+
+    def isfile(self, path):
+        return self.it_is_a_file == path
+
+    def path(self, path):
+        p = self.Path(path)
+        p.it_is_a_block_device = self.it_is_a_block_device
+        return p
+
+    def test_source_media_is_a_file(self, setup):
+        self.it_is_a_file = "./aFile.iso"
+        self.it_is_a_block_device = ["/dev/my_dev"]
+
+        result = WoeUSB.utils.check_runtime_parameters("device", "./aFile.iso", "/dev/my_dev")
+        pathlib.Path = self.path_preserve
+        assert result == 0
+
+    def test_source_media_is_a_block_device(self, setup):
+        self.it_is_a_file = ""
+        self.it_is_a_block_device = ["/dev/my_source", "/dev/my_dev"]
+
+        result = WoeUSB.utils.check_runtime_parameters("device", "/dev/my_source", "/dev/my_dev")
+        pathlib.Path = self.path_preserve
+        assert result == 0
+
+    def test_source_media_doesnt_exist(self, setup):
+        self.it_is_a_file = "./aFile.iso"
+        self.it_is_a_block_device = ["/dev/my_dev"]
+
+        result = WoeUSB.utils.check_runtime_parameters("device", "./aFile2.iso", "/dev/my_dev")
+        pathlib.Path = self.path_preserve
+        assert result == 1
+
+    def test_target_media_is_a_file(self, setup):
+        self.it_is_a_file = "./aFile.iso"
+        self.it_is_a_block_device = ["/dev/my_dev"]
+
+        result = WoeUSB.utils.check_runtime_parameters("device", "./aFile.iso", "./aFile.iso")
+        pathlib.Path = self.path_preserve
+        assert result == 1
+
+    def test_device_mode_target_media_isnt_whole_device(self, setup):
+        self.it_is_a_file = "./aFile.iso"
+        self.it_is_a_block_device = ["/dev/my_dev1"]
+
+        result = WoeUSB.utils.check_runtime_parameters("device", "./aFile.iso", "/dev/my_dev1")
+        pathlib.Path = self.path_preserve
+        assert result == 1
+
+    def test_partition_mode_target_media_is_whole_device(self, setup):
+        self.it_is_a_file = "./aFile.iso"
+        self.it_is_a_block_device = ["/dev/my_dev"]
+
+        result = WoeUSB.utils.check_runtime_parameters("partition", "./aFile.iso", "/dev/my_dev")
+        pathlib.Path = self.path_preserve
+        assert result == 1
+
+    def test_partition_mode_target_media_isnt_whole_device(self, setup):
+        self.it_is_a_file = "./aFile.iso"
+        self.it_is_a_block_device = ["/dev/my_dev1"]
+
+        result = WoeUSB.utils.check_runtime_parameters("partition", "./aFile.iso", "/dev/my_dev1")
+        pathlib.Path = self.path_preserve
+        assert result == 0
+
+
+class TestDetermineTargetParameters:
+    def test_device(self):
+        [target_device, target_partition] = WoeUSB.utils.determine_target_parameters("device", "/dev/sda")
+        assert {target_device, target_partition} \
+               .difference(["/dev/sda", "/dev/sda1"]) == set()
+
+    def test_partition(self):
+        [target_device, target_partition] = WoeUSB.utils.determine_target_parameters("partition", "/dev/sda1")
+        assert {target_device, target_partition} \
+               .difference(["/dev/sda", "/dev/sda1"]) == set()
 
 
 class TestCheckFat32FilesizeLimitation:

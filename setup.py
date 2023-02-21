f9d54c9857773a95f6021eb4772173ec3aafe69a
Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
2023-02-21 10:58:46 +0530

    fix python packaging using latest methods
    - Change python packaging method following instructions from
      https://packaging.python.org/en/latest/tutorials/packaging-projects
    - Remove scripts as they can be created automatically from setup.cfg
    - move the sources to src
    - setup.py: remove captialization in package names, conflicts with other
      packaging methods like debian's
    - modify licence to gpl3+
    - Add setup.cfg and pyproject.toml
    - modify MANIFEST.in to include assets
    - WoeUSB-ng.desktop: add version and change icon; fixes #11

--- setup.py
+++ setup.py
@@ -1,73 +1,13 @@
-import os
-import shutil
-import stat
-
 from setuptools import setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-
-this_directory = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
-def post_install():
-    path = '/usr/local/bin/woeusbgui'  # I give up, I have no clue how to get bin path that is used by pip
-    shutil.copy2(this_directory + '/WoeUSB/woeusbgui', path)  # I'll just hard code it until someone finds better way
-
-    shutil.copy2(this_directory + '/miscellaneous/com.github.woeusb.woeusb-ng.policy', "/usr/share/polkit-1/actions")
-
-    try:
-        os.makedirs('/usr/share/icons/WoeUSB-ng')
-    except FileExistsError:
-        pass
-
-    shutil.copy2(this_directory + '/WoeUSB/data/icon.ico', '/usr/share/icons/WoeUSB-ng/icon.ico')
-    shutil.copy2(this_directory + '/miscellaneous/WoeUSB-ng.desktop', "/usr/share/applications/WoeUSB-ng.desktop")
-
-    os.chmod('/usr/share/applications/WoeUSB-ng.desktop',
-             stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH | stat.S_IEXEC)  # 755
-
-
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-
-    def run(self):
-        # TODO
-        develop.run(self)
-
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-
-    def run(self):
-        post_install()
-        install.run(self)
-
 
 setup(
-    name='WoeUSB-ng',
-    version='0.2.12',
-    description='WoeUSB-ng is a simple tool that enable you to create your own usb stick windows installer from an iso image or a real DVD. This is a rewrite of original WoeUSB. ',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/WoeUSB/WoeUSB-ng',
-    author='Jakub Szymański',
-    author_email='jakubmateusz@poczta.onet.pl',
-    license='GPL-3',
-    zip_safe=False,
-    packages=['WoeUSB'],
-    include_package_data=True,
-    scripts=[
-        'WoeUSB/woeusb',
+    data_files=[
+        ("share/applications", ["miscellaneous/WoeUSB-ng.desktop"]),
+        ("share/polkit-1/actions", ["miscellaneous/com.github.woeusb.woeusb-ng.policy"]),
+        ("share/icons/hicolor/scalable/apps", ["src/WoeUSB/data/woeusb-logo.png"]),
     ],
     install_requires=[
         'termcolor',
         'wxPython',
-    ],
-    cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand
-    }
+    ]
 )
